8ad048d2ae214c3cb5dc72741bec677334a112ee
Thu Dang <thudang.dtt@gmail.com>
2021-08-15 13:08:03 -0500

    js fixes

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const form = document.getElementsByClassName("form");
-const inputs = [...form.getElementByTagName("input")];
-//   regex = /\S+@\S+\.\S+/;
+const form = document.getElementsByClassName("form")[0];
+const inputs = [...form.getElementsByTagName("input")];
+const regex = /\S+@\S+\.\S+/;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   inputs.forEach((input) => {
-    if (input.value == "") {
+    if (
+      input.value == "" ||
+      (input.name === "email" && !regex.test(input.value))
+    ) {
       input.classList.add("show-icon");
       input.nextElementSibling.classList.add("show-error");
       input.nextElementSibling.classList.remove("error");
@@ -15,7 +18,5 @@ form.addEventListener("submit", (e) => {
       input.nextElementSibling.classList.remove("show-error");
       input.nextElementSibling.classList.add("error");
     }
-    // if (input.name === "email" && !regex.test(input.value)) {
-    // ...}
   });
 });
